54010049d144efe8d106a748b630acfd6d60611b
Eero Gallano <gallanoeero@gmail.com>
2026-03-13 13:30:57 -0700

    chore: replace React vite plugin with Svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [svelte(), tailwindcss()],
   clearScreen: false,
   server: {
     port: 1420,
